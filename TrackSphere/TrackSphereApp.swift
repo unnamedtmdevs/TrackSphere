0fddc9c10e6ca352eb84d9a0b5ba0832153215b5
ludik <bakhanetssimon@gmail.com>
2025-12-18 22:15:47 +0700

    Initial Commit

---
+++ TrackSphere/TrackSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackSphereApp.swift
+//  TrackSphere
+//
+//  Created by Simon Bakhanets on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrackSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackSphereTests/TrackSphereTests.swift
@@ -0,0 +1,16 @@
+//
+//  TrackSphereTests.swift
+//  TrackSphereTests
+//
+//  Created by Simon Bakhanets on 18.12.2025.
+//
+
+import Testing
+
+struct TrackSphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
